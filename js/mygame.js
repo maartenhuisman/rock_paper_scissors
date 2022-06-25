72d7d609a0f1edc502cf7d186a85fb2164b8acaf
Maarten Huisman <mrtn.hsmn@gmail.com>
2022-06-25 21:52:16 +0200

    Add function playRound

--- js/mygame.js
+++ js/mygame.js
@@ -2,12 +2,33 @@ console.log("Hello player!");
 console.log("Let's play a round of Rock paper scissors")
 
 function computerPlay() {
-    const computerOptions = ["rock", "paper", "scissors"];
-    let computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    return computerSelection;
-    
+    const computerOptions = ["Rock", "Paper", "Scissors"];
+    const computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    return computerChoice;
 }
 
-console.log(computerPlay())
+//  console.log(`The computer chose: ${computerPlay()}`);
 
-function
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        let winState = "Draw!";
+        return winState
+    } else if (
+        playerSelection === "Rock" && computerSelection === "Scissors" ||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper"
+        ) {
+        console.log(`computer: ${computerSelection}`);
+        let winState = `You won! ${playerSelection} beats ${computerSelection}.`;
+        return winState
+    } else {
+        let winState = `You lost! ${computerSelection} beats ${playerSelection}.`;
+        return winState
+    }
+}
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+
+console.log(`You chose: ${playerSelection}`);
+console.log(`The computer chose: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
