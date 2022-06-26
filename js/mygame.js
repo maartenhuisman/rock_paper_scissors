aa34351fd9f8aa234cd6d34f32407c6d5da68adf
Maarten Huisman <mrtn.hsmn@gmail.com>
2022-06-26 20:27:22 +0200

    Add capitalize function.

--- js/mygame.js
+++ js/mygame.js
@@ -1,5 +1,5 @@
 console.log("Hello player!");
-console.log("Let's play a round of Rock paper scissors")
+console.log("Let's play 5 rounds of Rock paper scissors")
 
 function computerPlay() {
     const computerOptions = ["Rock", "Paper", "Scissors"];
@@ -10,33 +10,38 @@ function computerPlay() {
 let string = prompt("Please choose Rock, Paper or Scissors");
 
 function capitalize(string) {
-  newString = string.toLowerCase()
-  uncapitalizedEnd = newString.slice(1)
-  capitalLetter = string.charAt(0)
-  newString2 = capitalLetter.toUpperCase() + uncapitalizedEnd
-
-  return newString2
+    newString = string.toLowerCase();
+    uncapitalizedEnd = newString.slice(1);
+    capitalLetter = string.charAt(0);
+    capitalizedString = capitalLetter.toUpperCase() + uncapitalizedEnd;
+    return capitalizedString;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         let winState = "Draw!";
-        return winState
+        return winState;
     } else if (
         playerSelection === "Rock" && computerSelection === "Scissors" ||
         playerSelection === "Paper" && computerSelection === "Rock" ||
         playerSelection === "Scissors" && computerSelection === "Paper"
         ) {
         let winState = `You won! ${playerSelection} beats ${computerSelection}.`;
-        return winState
+        return winState;
     } else {
         let winState = `You lost! ${computerSelection} beats ${playerSelection}.`;
-        return winState
+        return winState;
     }
 }
-const playerSelection = capitalize(string);
-const computerSelection = computerPlay();
 
-console.log(`You chose: ${playerSelection}`);
-console.log(`The computer chose: ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+//    for (let i = 0; i < 5; i++) {
+        const playerSelection = capitalize(string);
+        const computerSelection = computerPlay();
+        console.log(`You chose: ${playerSelection}`);
+        console.log(`The computer chose: ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+//}
+
+game();
