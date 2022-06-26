8da3185b2d7e75a8a3d92b62f38f8d6b7b88d9a5
Maarten Huisman <mrtn.hsmn@gmail.com>
2022-06-26 20:40:51 +0200

    Add play 5 rounds loop.

--- js/mygame.js
+++ js/mygame.js
@@ -7,8 +7,6 @@ function computerPlay() {
     return computerChoice;
 }
 
-let string = prompt("Please choose Rock, Paper or Scissors");
-
 function capitalize(string) {
     newString = string.toLowerCase();
     uncapitalizedEnd = newString.slice(1);
@@ -35,13 +33,15 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-//    for (let i = 0; i < 5; i++) {
-        const playerSelection = capitalize(string);
-        const computerSelection = computerPlay();
+    for (let i = 0; i < 5; i++) {
+        let string = prompt("Please choose Rock, Paper or Scissors");
+        let playerSelection = capitalize(string);
+        let computerSelection = computerPlay();
         console.log(`You chose: ${playerSelection}`);
         console.log(`The computer chose: ${computerSelection}`);
         console.log(playRound(playerSelection, computerSelection));
     }
-//}
+    console.log("Game over man.");
+}
 
 game();
